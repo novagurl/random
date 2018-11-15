387ec6b7e482c6dece2b9922675d5d5b78649307
novagurl <novagurl@users.noreply.github.com>
2018-11-15 10:34:06 -0400

    ofApp.cpp

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -5,23 +5,32 @@ using namespace std;
 //--------------------------------------------------------------
 void ofApp::setup(){
 jewel.load("diamond.png");// image to use (must be stored in "random/bin/data" folder)
-X = {500, 1000, 30, 100, 300, 200, 750, 380 };// set of x-coordinates for jewel
-Y = {630, 400, 900,40, 200, 0, 90, 30}; //set of y-coordinates for jewel
+//X = {500, 1000, 30, 100, 300, 200, 750, 380 };// set of x-coordinates for jewel
+//Y = {630, 400, 900, 40, 200, 0, 90, 30}; //set of y-coordinates for jewel
 	
 	random_device rd;
 	mt19937 eng1(rd());
 	
-	//uniform_int_distribution <int> jewel_x(0, 1200);
-    //uniform_int_distribution <int> jewel_y(0, 970);
+	/*Uniform Distribution
+	uniform_int_distribution < > jewel_x(0, 1200);
+    uniform_int_distribution < > jewel_y(0, 970);*/
     
-    normal_distribution <float> jewel_x(1015, 985);
-    normal_distribution <float> jewel_y(815, 785);
+    
+    //Normal Distribution
+    normal_distribution <float > jewel_x(1000, 15);
+    normal_distribution < float> jewel_y(800, 15);
    
 
-	for(int n=0 ;  n < 20 ;  n++) 
+	for(int n = 0 ;  n < 20 ;  n++) 
     {
 		X.push_back(jewel_x (eng1));
+		cout<< jewel_x(eng1)<<endl;
+	}
+	
+	for(int n = 0; n < 20; n++)
+	{
 		Y.push_back(jewel_y (eng1));
+		cout<< jewel_y(eng1)<<endl;
 	}
 }
 
